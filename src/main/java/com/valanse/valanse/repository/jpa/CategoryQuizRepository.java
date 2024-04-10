4695c34d20b97a2747a3fefb997c61b43c069620
00asha <qwer2pa@naver.com>
2024-04-10 23:59:01 +0900

    renamed repository

--- src/main/java/com/valanse/valanse/repository/jpa/CategoryQuizRepository.java
+++ src/main/java/com/valanse/valanse/repository/jpa/CategoryQuizRepository.java
@@ -2,5 +2,5 @@ package com.valanse.valanse.repository.jpa;
 import com.valanse.valanse.entity.CategoryQuiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryQuestionRepository extends JpaRepository<CategoryQuiz, Integer> {
+public interface CategoryQuizRepository extends JpaRepository<CategoryQuiz, Integer> {
 }
--- src/main/java/com/valanse/valanse/repository/jpa/UserAnswerRepository.java
+++ src/main/java/com/valanse/valanse/repository/jpa/UserAnswerRepository.java
@@ -3,5 +3,5 @@ package com.valanse.valanse.repository.jpa;
 import com.valanse.valanse.entity.UserAnswer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientResponseRepository extends JpaRepository<UserAnswer, Integer> {
+public interface UserAnswerRepository extends JpaRepository<UserAnswer, Integer> {
 }
