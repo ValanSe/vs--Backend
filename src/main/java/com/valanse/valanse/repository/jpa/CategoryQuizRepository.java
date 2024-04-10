41725c880c85f7400b2dd45b8e308abf037a9c6b
00asha <qwer2pa@naver.com>
2024-04-11 00:06:02 +0900

    interface revice

--- src/main/java/com/valanse/valanse/repository/jpa/CategoryQuizRepository.java
+++
@@ -1,6 +0,0 @@
-package com.valanse.valanse.repository.jpa;
-import com.valanse.valanse.entity.CategoryQuiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryQuestionRepository extends JpaRepository<CategoryQuiz, Integer> {
-}
