a3aab2320cf99e8154ff5b2b7ab5abd86153c9ef
00asha <qwer2pa@naver.com>
2024-04-17 02:10:27 +0900

    interface revise

--- src/main/java/com/valanse/valanse/service/CategoryService/CategoryService.java
+++ src/main/java/com/valanse/valanse/service/CategoryService/CategoryService.java
@@ -8,9 +8,8 @@ import java.util.Optional;
 
 public interface CategoryService {
     void addQuizToCategory(Category category, Quiz quiz); // 카테고리에 질문 추가
-    void removeQuizFromCategory(Category category, Quiz quiz); // 카테고리에서 질문 제거
     List<Category> searchCategory(String keyword); // 카테고리 검색
-    Optional<Integer> findQuizCountByCategory(Category category); // 카테고리에 속한 퀴즈 수 조회
-    Optional<Double> findAveragePreferenceByCategory(Category category); // 카테고리에 속한 퀴즈의 평균 선호도 조회
-    Optional<Integer> findRecentActivityByCategory(Category category); // 카테고리에 속한 퀴즈의 최근 활동(댓글 수, 조회수 등) 조회
+    Optional<Integer> getQuizCountByCategory(Category category); // 카테고리에 속한 퀴즈 수 조회
+    Optional<Double> getAveragePreferenceByCategory(Category category); // 카테고리에 속한 퀴즈의 평균 선호도 조회
+    Optional<Integer> getRecentActivityByCategory(Category category); // 카테고리에 속한 퀴즈의 최근 활동(댓글 수, 조회수 등) 조회
 }
