c28f078067c1f78c73f73235c7a625bf47eb31d6
조재중 <126754298+m-a-king@users.noreply.github.com>
2024-05-19 21:02:19 +0900

    [fix: AwsS3Config]

--- src/main/java/com/valanse/valanse/config/AwsS3Config.java
+++ src/main/java/com/valanse/valanse/config/AwsS3Config.java
@@ -12,13 +12,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AwsS3Config {
 
-    @Value("cloud.aws.credentials.accessKey")
+    @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
 
-    @Value("cloud.aws.credentials.secretKey")
+    @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
-    @Value("cloud.aws.region.static")
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
