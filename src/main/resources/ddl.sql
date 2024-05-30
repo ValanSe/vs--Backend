ccd2462c35ba76256ba1fd3a28c161000edef472
00asha <qwer2pa@naver.com>
2024-05-31 00:44:58 +0900

    pull 전 commit

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -50,6 +50,8 @@ CREATE TABLE `quiz`
     `image_b`        TEXT COMMENT 'B 이미지',
     `view`           INT          NOT NULL COMMENT '조회수',
     `preference`     INT          NOT NULL COMMENT '선호도 수',
+    `like_count`     INT          DEFAULT 0,
+    `unlike_count`   INT          DEFAULT 0,
     `created_at`     DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '질문 생성 시간',
     `updated_at`     DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '질문 수정 시간',
     FOREIGN KEY (`author_user_id`) REFERENCES `user` (`user_id`)
@@ -62,7 +64,7 @@ CREATE TABLE `user_answer`
     `answer_id`        INT AUTO_INCREMENT PRIMARY KEY COMMENT '답변 식별자',
     `user_id`          INT      NOT NULL COMMENT '답변한 사용자 식별자',
     `quiz_id`          INT      NOT NULL COMMENT '답변한 질문 식별자',
-    `selected_option`  VARCHAR(255) COMMENT '선택된 옵션 (시간 내 선택하지 못한 경우 NULL)',
+    `selected_option`  VARCHAR(255) COMMENT '선택된 옵션',
     `answered_at`      DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '답변 시간',
     `time_spent`       INT      NOT NULL COMMENT '답변에 걸린 시간(초)',
     `preference`       INT      NOT NULL COMMENT '문제에 대한 사용자의 호감도',
@@ -79,6 +81,39 @@ CREATE TABLE `quiz_category`
     `category` VARCHAR(100) NOT NULL COMMENT '카테고리 식별자',
     PRIMARY KEY (`quiz_id`, `category`),
     FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`quiz_id`) ON DELETE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE `Notice` (
+    notice_id INT AUTO_INCREMENT PRIMARY KEY COMMENT '공지사항 식별자',
+    title VARCHAR(255) NOT NULL COMMENT '제목',
+    content TEXT NOT NULL COMMENT '내용',
+    author_id INT NOT NULL COMMENT '공지사항을 등록한 관리자 식별자',
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '공지사항 생성 시간',
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '공지사항 수정 시간',
+    views INT DEFAULT 0 COMMENT '조회수',
+    FOREIGN KEY (author_id) REFERENCES `user` (user_id) ON DELETE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE Comment (
+    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_user_id INT NOT NULL,
+    content VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE `comment_quiz` (
+    `quiz_id` INT NOT NULL,
+    `comment_id`  INT NOT NULL,
+    PRIMARY KEY (`quiz_id`, `comment_id`),
+    FOREIGN KEY (`quiz_id`) REFERENCES `quiz` (`quiz_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`comment_id`) REFERENCES `comment` (`comment_id`) ON DELETE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_general_ci;
