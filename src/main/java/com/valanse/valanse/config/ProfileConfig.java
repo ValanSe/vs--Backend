df10bebdadecbf2d9a2184de8ce9576bb55561e4
00asha <qwer2pa@naver.com>
2024-05-31 03:16:24 +0900

    UserPreferenceRepository 추가

--- src/main/java/com/valanse/valanse/config/ProfileConfig.java
+++ src/main/java/com/valanse/valanse/config/ProfileConfig.java
@@ -3,6 +3,7 @@ package com.valanse.valanse.config;
 import com.valanse.valanse.repository.jpa.QuizCategoryRepository;
 import com.valanse.valanse.repository.jpa.QuizRepository;
 import com.valanse.valanse.repository.jpa.UserAnswerRepository;
+import com.valanse.valanse.repository.jpa.UserPreferenceRepository;
 import com.valanse.valanse.security.util.JwtUtil;
 import com.valanse.valanse.service.ImageService.S3ImageService;
 import com.valanse.valanse.service.QuizService.QuizService;
@@ -20,12 +21,14 @@ public class ProfileConfig {
     public QuizService quizService(QuizRepository quizRepository,
                                    QuizCategoryRepository quizCategoryRepository,
                                    UserAnswerRepository userAnswerRepository,
+                                   UserPreferenceRepository userPreferenceRepository,
                                    S3ImageService s3ImageService,
                                    JwtUtil jwtUtil) {
 
         return new QuizServiceImpl(quizRepository,
                 quizCategoryRepository,
                 userAnswerRepository,
+                userPreferenceRepository,
                 s3ImageService,
                 jwtUtil);
     }
