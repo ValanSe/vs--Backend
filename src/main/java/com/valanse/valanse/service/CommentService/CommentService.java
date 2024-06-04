8d5bfd0052c08cfa4229e3fa6896ce5cbf05213a
00asha <qwer2pa@naver.com>
2024-06-04 21:03:53 +0900

    getCommentByQuizId로 메서드명 수정
    updateComment parameter commentUpdateDto에서 content로 수정

--- src/main/java/com/valanse/valanse/service/CommentService/CommentService.java
+++ src/main/java/com/valanse/valanse/service/CommentService/CommentService.java
@@ -1,16 +1,20 @@
 package com.valanse.valanse.service.CommentService;
 
 import com.valanse.valanse.dto.CommentDto;
+import com.valanse.valanse.dto.CommentQuizDto;
 import com.valanse.valanse.dto.CommentRegisterDto;
-import com.valanse.valanse.dto.CommentUpdateDto;
 import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
 
 public interface CommentService {
     void registerComment(HttpServletRequest httpServletRequest, CommentRegisterDto commentRegisterDto);
 
     CommentDto getComment(Integer commentId);
 
-    void updateComment(HttpServletRequest httpServletRequest, Integer commentId, CommentUpdateDto commentUpdateDto);
+    List<CommentQuizDto> getCommentByQuizId(Integer quizId);
+
+    void updateComment(HttpServletRequest httpServletRequest, Integer commentId, String content);
 
     void deleteComment(HttpServletRequest httpServletRequest, Integer commentId);
 }
