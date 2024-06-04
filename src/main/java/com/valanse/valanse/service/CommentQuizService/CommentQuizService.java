711a01c43b4ff6e9749fd0f147db31bcc47548ab
00asha <qwer2pa@naver.com>
2024-06-04 21:01:43 +0900

    CommentQuiz Service와 Controller 삭제하고 Comment Service와 Controller로 이동

--- src/main/java/com/valanse/valanse/service/CommentQuizService/CommentQuizService.java
+++
@@ -1,9 +0,0 @@
-package com.valanse.valanse.service.CommentQuizService;
-
-import com.valanse.valanse.dto.CommentQuizDto;
-
-import java.util.List;
-
-public interface CommentQuizService {
-    List<CommentQuizDto> getCommentByQuiz(Integer quizId);
-}
