bbd2ace5e48b20e37c81a9a52e484c5e518f2f3b
00asha <qwer2pa@naver.com>
2024-06-07 18:33:17 +0900

    CategoryStatistics 추가

---
+++ src/main/java/com/valanse/valanse/repository/jpa/CategoryStatisticsRepository.java
@@ -0,0 +1,7 @@
+package com.valanse.valanse.repository.jpa;
+
+import com.valanse.valanse.entity.CategoryStatistics;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryStatisticsRepository extends JpaRepository<CategoryStatistics, String> {
+}
