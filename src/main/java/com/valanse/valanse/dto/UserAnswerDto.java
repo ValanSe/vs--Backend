ecf2910c8bc163371c21d54f056ece81098af770
00asha <qwer2pa@naver.com>
2024-06-08 06:03:33 +0900

    UserAnswerDto 수정
    answeredAt 삭제
    likeCount, unlikeCount 추가

--- src/main/java/com/valanse/valanse/dto/UserAnswerDto.java
+++ src/main/java/com/valanse/valanse/dto/UserAnswerDto.java
@@ -13,6 +13,7 @@ public class UserAnswerDto {
     private Integer userId; // 답변한 사용자 식별자
     private Integer quizId; // 답변한 퀴즈 식별자
     private String selectedOption; // 선택한 옵션
-    private LocalDateTime answeredAt; // 답변한 시간
     private Integer preference; // 퀴즈에 대한 선호도
+    private Integer likeCount; // 퀴즈의 좋아요 수
+    private Integer unlikeCount; // 퀴즈의 싫어요 수
 }
