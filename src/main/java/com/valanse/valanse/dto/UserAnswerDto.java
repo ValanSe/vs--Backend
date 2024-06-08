185114e64e951cae0c3c2a97f5b4c0db1a695115
00asha <qwer2pa@naver.com>
2024-06-09 00:42:18 +0900

    likeCount, unlikeCount Column 삭제

--- src/main/java/com/valanse/valanse/dto/UserAnswerDto.java
+++ src/main/java/com/valanse/valanse/dto/UserAnswerDto.java
@@ -14,6 +14,4 @@ public class UserAnswerDto {
     private Integer quizId; // 답변한 퀴즈 식별자
     private String selectedOption; // 선택한 옵션
     private Integer preference; // 퀴즈에 대한 선호도
-    private Integer likeCount; // 퀴즈의 좋아요 수
-    private Integer unlikeCount; // 퀴즈의 싫어요 수
 }
