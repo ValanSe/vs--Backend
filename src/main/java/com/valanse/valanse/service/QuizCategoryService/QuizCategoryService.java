469f3aa42eedf8fe55fa8c2551aad6f99bdb0ee2
00asha <qwer2pa@naver.com>
2024-06-09 02:13:56 +0900

    getStatsByCategory 메서드 삭제

--- src/main/java/com/valanse/valanse/service/QuizCategoryService/QuizCategoryService.java
+++ src/main/java/com/valanse/valanse/service/QuizCategoryService/QuizCategoryService.java
@@ -1,17 +1,12 @@
 package com.valanse.valanse.service.QuizCategoryService;
 
 import com.valanse.valanse.dto.QuizCategoryDto;
-import com.valanse.valanse.dto.QuizCategoryStatsDto;
-import com.valanse.valanse.entity.QuizCategory;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface QuizCategoryService {
 
     List<QuizCategoryDto> getAllQuizByCategory(String category);
 
     List<QuizCategoryDto> searchCategory(String keyword); // 카테고리 검색
-
-    QuizCategoryStatsDto getStatsByCategory(String category); // 카테고리에 속한 퀴즈 수, 카테고리에 속한 퀴즈의 조회수 합, 평균 선호도 수 조회
 }
--- src/main/java/com/valanse/valanse/service/QuizCategoryService/QuizCategoryServicelmpl.java
+++ src/main/java/com/valanse/valanse/service/QuizCategoryService/QuizCategoryServicelmpl.java
@@ -1,12 +1,7 @@
 package com.valanse.valanse.service.QuizCategoryService;
 
 import com.valanse.valanse.dto.QuizCategoryDto;
-import com.valanse.valanse.dto.QuizCategoryStatsDto;
-import com.valanse.valanse.entity.Quiz;
-import com.valanse.valanse.entity.QuizCategory;
 import com.valanse.valanse.repository.jpa.QuizCategoryRepository;
-import com.valanse.valanse.repository.jpa.QuizRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,7 +15,6 @@ import java.util.stream.Collectors;
 public class QuizCategoryServicelmpl implements QuizCategoryService {
 
     private final QuizCategoryRepository quizCategoryRepository;
-    private final QuizRepository quizRepository;
 
     @Override
     public List<QuizCategoryDto> getAllQuizByCategory(String category) {
@@ -40,36 +34,5 @@ public class QuizCategoryServicelmpl implements QuizCategoryService {
                         .quizId(quizCategory.getQuizId())
                         .build())
                 .collect(Collectors.toList());
-    }
-
-    @Override
-    public QuizCategoryStatsDto getStatsByCategory(String category) {
-        try {
-            List<QuizCategory> quizzesInCategory = quizCategoryRepository.findByCategory(category);
-
-            if (quizzesInCategory.isEmpty()) {
-                throw new EntityNotFoundException("Quiz Category not found: " + category);
-            }
-
-            int totalView = 0;
-            double totalPreference = 0.0;
-
-            for (QuizCategory quizCategory : quizzesInCategory) {
-                Quiz quiz = quizRepository.findById(quizCategory.getQuizId()).orElseThrow(EntityNotFoundException::new);
-
-                totalView += quiz.getViewCount();
-                totalPreference += quiz.getPreference();
-            }
-
-            return QuizCategoryStatsDto.builder()
-                    .quizCount(quizzesInCategory.size())
-                    .viewCount(totalView)
-                    .averagePreference(totalPreference / quizzesInCategory.size())
-                    .build();
-
-        } catch (EntityNotFoundException e) {
-            log.error("{} not found", category, e);
-            throw e;
-        }
     }
 }
